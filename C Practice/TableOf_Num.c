779c0b1b36d4fa23648978041caf9a85b5b461ee
Pratham-Techops <88659230+Pratham-Techops@users.noreply.github.com>
2021-12-26 16:39:31 +0530

    h

---
+++ C Practice/TableOf_Num.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int num;
+    printf("Enter a number: \n");
+    scanf("%d",&num);
+    for(int i=1; i<=10; i++){
+        printf("%d * %d = %d\n", num, i, num*i);
+    }
+    return 0;
+}
